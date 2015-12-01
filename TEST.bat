13d5983b2daae5c89fdd74ba70b9b117ddd1e94b
darkrain7 <darkrain7@mail.ru>
2015-12-01 16:15:02 +0700

    pognali
    leha

--- TEST.bat
+++ TEST.bat
@@ -1,17 +1,44 @@
-java -classpath "commons-cli-1.3.1.jar;AAAproj.jar" Main  -h
-java -classpath "commons-cli-1.3.1.jar;AAAproj.jar" Main  -login XXX -pass XXX
-java -classpath "commons-cli-1.3.1.jar;AAAproj.jar" Main  -login jdoe -pass XXX
-java -classpath "commons-cli-1.3.1.jar;AAAproj.jar" Main  -login jdoe -pass sup3rpaZZ
+@echo off 
+call RUN.bat 
+(echo %ERRORLEVEL%) 
 
-java -classpath "commons-cli-1.3.1.jar;AAAproj.jar" Main  -login jdoe -pass sup3rpaZZ -role READ -res a
-java -classpath "commons-cli-1.3.1.jar;AAAproj.jar" Main  -login jdoe -pass sup3rpaZZ -role READ -res a.b
-java -classpath "commons-cli-1.3.1.jar;AAAproj.jar" Main  -login jdoe -pass sup3rpaZZ -role XXX -res a.b
-java -classpath "commons-cli-1.3.1.jar;AAAproj.jar" Main  -login jdoe -pass sup3rpaZZ -role READ -res XXX
-java -classpath "commons-cli-1.3.1.jar;AAAproj.jar" Main  -login jdoe -pass sup3rpaZZ -role WRITE -res a
-java -classpath "commons-cli-1.3.1.jar;AAAproj.jar" Main  -login jdoe -pass sup3rpaZZ -role WRITE -res a.bc
+call RUN.bat -h 
+(echo %ERRORLEVEL%) 
 
-java -classpath "commons-cli-1.3.1.jar;AAAproj.jar" Main  -login jdoe -pass sup3rpaZZ -role WRITE -res a.b -ds 2015-01-01 -de 2015-12-31 -vol 100
-java -classpath "commons-cli-1.3.1.jar;AAAproj.jar" Main  -login jdoe -pass sup3rpaZZ -role WRITE -res a.b -ds 01-01-2015 -de 2015-12-31 -vol 100
-java -classpath "commons-cli-1.3.1.jar;AAAproj.jar" Main  -login jdoe -pass sup3rpaZZ -role WRITE -res a.b -ds 2015-01-01 -de 2015-12-31 -vol XXX
+call RUN.bat -login XXX -pass XXX 
+(echo %ERRORLEVEL%) 
+
+call RUN.bat -login jdoe -pass XXX 
+(echo %ERRORLEVEL%) 
+
+call RUN.bat -login jdoe -pass sup3rpaZZ 
+(echo %ERRORLEVEL%) 
+
+call RUN.bat -login jdoe -pass sup3rpaZZ -role READ -res a 
+(echo %ERRORLEVEL%) 
+
+call RUN.bat -login jdoe -pass sup3rpaZZ -role READ -res a.b 
+(echo %ERRORLEVEL%) 
+
+call RUN.bat -login jdoe -pass sup3rpaZZ -role XXX -res a.b 
+(echo %ERRORLEVEL%) 
+
+call RUN.bat -login jdoe -pass sup3rpaZZ -role READ -res XXX 
+(echo %ERRORLEVEL%) 
+
+call RUN.bat -login jdoe -pass sup3rpaZZ -role WRITE -res a 
+(echo %ERRORLEVEL%) 
+
+call RUN.bat -login jdoe -pass sup3rpaZZ -role WRITE -res a.bc 
+(echo %ERRORLEVEL%) 
+
+call RUN.bat -login jdoe -pass sup3rpaZZ -role READ -res a.b -ds 2015-01-01 -de 2015-12-31 -vol 100 
+(echo %ERRORLEVEL%) 
+
+call RUN.bat -login jdoe -pass sup3rpaZZ -role READ -res a.b -ds 01-01-2015 -de 2015-12-31 -vol 100 
+(echo %ERRORLEVEL%) 
+
+call RUN.bat -login jdoe -pass sup3rpaZZ -role READ -res a.b -ds 2015-01-01 -de 2015-12-31 -vol XXX 
+(echo %ERRORLEVEL%) 
 
 pause
